720b24027a821ae22f3873d8132d3d739398f9ba
Rob Erskine <Rob@RobErskine.com>
2015-11-13 11:51:45 -0500

    adding lowercase folder back in

---
+++ usermanual/twigextensions/UserManualTwigExtension.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Craft;
+
+use Twig_Extension;  
+use Twig_Function_Method;
+use Twig_Filter_Method;
+
+class UserManualTwigExtension extends Twig_Extension  
+{
+  public function getName()
+  {
+      return 'User Manual Twig Extension';
+  }
+
+  public function getFunctions()
+  {
+    return array(
+      'getHelpDocument' => new Twig_Function_Method($this, 'getHelpDocument'),
+    );
+  }
+
+  public function getHelpDocument($path = '') {
+    $segments = craft()->request->segments;
+    $segment = end($segments);
+    
+    $section = craft()->userManual_settings->getChannelSetting();
+
+    $criteria = craft()->elements->getCriteria(ElementType::Entry);
+    $criteria->section = $section;
+    $criteria->slug = $segment;
+    $entry = $criteria->find();
+
+  // if the app has not been set up at all, redirect to the settings page
+    if($section == ''){
+      craft()->request->redirect(UrlHelper::getCpUrl('settings/plugins/usermanual/'));
+    }
+  // if there is no url segment, load the homepage
+    else if(! $entry){
+      $firstCriteria = craft()->elements->getCriteria(ElementType::Entry);
+      $firstCriteria->section = $section;
+      $firstCriteria->limit = 1;
+      $firstEntry = $firstCriteria->find();
+      echo($firstEntry[0]['body']);
+    }
+  // if there is a defined page, load that page's body
+    else{
+      echo($entry[0]['body']);
+    }
+  }
+}
+
+?>
