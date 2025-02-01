12e52bbcc3afed29f37c1beb887db39c608879f7
Jorge Anzola <jorgeanzola@gmail.com>
2025-02-01 13:25:26 +0100

    Removed URL requirement

---
+++ src/translations/es/usermanual.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * usermanual plugin for Craft CMS 4.x / 5.x
+ *
+ * Craft User Manual allows developers (or even content editors) to provide CMS
+ * documentation using Craft's built-in sections (singles, channels, or structures)
+ * to create a `User Manual` or `Help` section directly in the control panel.
+ *
+ * @link      https://twitter.com/erskinerob
+ * @copyright Copyright (c) 2018 Rob Erskine
+ */
+
+/**
+ * @author    Rob Erskine
+ * @package   Usermanual
+ * @since     2.0.0
+ */
+return [
+    'User Manual' => 'Manual del usuario',
+    'usermanual plugin loaded' => 'Manual cargado',
+    'no section error' => 'No hay ninguna sección seleccionada para el plugin. Por favor visita la página de configuración',
+    'no section error for config file' => 'No hay ninguna sección válifa para el plugin. Por favor revisa el archivo de configuración',
+    'no entry error' => 'No hay entradas en la sección seleccionada para el plguin. Por favor revisa la página de configuraciones. Las entradas deben tener slug para poder mostrarse.',
+    'config file section error' => 'La sección debe ser un string o un integer.',
+];
